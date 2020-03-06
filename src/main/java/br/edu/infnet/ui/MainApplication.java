a663abe9d24ef895403f9f1593d70bac5cd142dc
Fábio Pereira <fabiopereira.fps@gmail.com>
2020-03-06 18:31:22 -0300

    Change layout default width and height

--- src/main/java/br/edu/infnet/ui/MainApplication.java
+++ src/main/java/br/edu/infnet/ui/MainApplication.java
@@ -7,15 +7,16 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainApplication extends Application {
+    private final int initialWidth = 500;
+    private final int initialHeight = 300;
+
     public static void main(String[] args) { launch(args); }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/views/Initial.fxml"));
-        var scene = new Scene(root, 562, 267);
-
         primaryStage.setTitle("Academic Control");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(new Scene(root, initialWidth, initialHeight));
         primaryStage.show();
     }
 }
