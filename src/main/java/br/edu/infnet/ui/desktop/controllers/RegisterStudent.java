ee02d35a7058974b63db62de0a6cfe5e89b4b87c
Fabio Pereira <fabiopereira.fps@gmail.com>
2020-03-09 03:07:24 -0300

    Add form double fields validation

--- src/main/java/br/edu/infnet/ui/desktop/controllers/RegisterStudent.java
+++ src/main/java/br/edu/infnet/ui/desktop/controllers/RegisterStudent.java
@@ -6,8 +6,11 @@ import br.edu.infnet.infrastructure.StudentsRepository;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.util.regex.Pattern;
 
 public class RegisterStudent {
     private final Repository<Student> studentsRepository = StudentsRepository.getInstance();
@@ -25,13 +28,36 @@ public class RegisterStudent {
     private Button confirmButton;
 
     @FXML
-    void registerStudent(ActionEvent event) {
+    private Label errorMessage;
+
+
+    @FXML
+    void registerStudent() {
         var stage = (Stage) confirmButton.getScene().getWindow();
+
+        if (isValidForm()) {
+            studentsRepository.add(createStudentFromFormInput());
+            stage.close();
+        } else {
+            errorMessage.setText("Valores não válidos.");
+        }
+    }
+
+    private boolean isValidForm() {
+        var firstGradeValue = firstGradeField.getText();
+        var secondGraveValue = secondGradeField.getText();
+        return validateDouble(firstGradeValue) && validateDouble(secondGraveValue);
+    }
+
+    private Student createStudentFromFormInput() {
         var name = nameField.getText();
         var firstGrade = Double.parseDouble(firstGradeField.getText());
         var secondGrade = Double.parseDouble(secondGradeField.getText());
-        studentsRepository.add(new Student(name, firstGrade, secondGrade));
+        return new Student(name, firstGrade, secondGrade);
+    }
 
-        stage.close();
+    private boolean validateDouble(String firstGradeValue) {
+        var DOUBLE_PATTERN = "[0-9]+(\\.)?[0-9]*";
+        return Pattern.matches(DOUBLE_PATTERN, firstGradeValue);
     }
 }
